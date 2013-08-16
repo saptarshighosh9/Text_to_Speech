55f3ec2461774a88dd3a915b352e0999e29c4135
saptarshighosh9 <saptarshighosh9@users.noreply.github.com>
2013-08-16 12:36:04 +0530

    Create T_speech_main.bat

---
+++ T_speech_main.bat
@@ -0,0 +1,10 @@
+@echo off
+color 0a
+echo *******************************************
+echo Txt to Speech Converter
+echo *******************************************
+echo by Gooblu
+echo have fun
+:ff
+start/wait speech.vbs
+goto ff
